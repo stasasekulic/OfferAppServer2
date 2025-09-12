4845dc101c36acb9df43b667c57cf78ee95c222a
Staša Sekulić <stasa.sekulic.91@gmail.com>
2025-09-12 22:28:31 +0200

    UserRepository implementation

---
+++ src/main/java/offerappserver/server/user/UserRepository.java
@@ -0,0 +1,9 @@
+package offerappserver.server.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
